ab4cb5dbf5dae3453489b307e188f421191598a3
Bence Meszaros <mxbence@gmail.com>
2013-01-10 14:02:04 +0100

    search for street type

--- src/JCSGYK/AdminBundle/Controller/SearchController.php
+++ src/JCSGYK/AdminBundle/Controller/SearchController.php
@@ -26,6 +26,9 @@ class SearchController extends Controller
             $time_start = microtime(true);
             if (!empty($q)) {
 
+                // TODO: we need to find a good location for this stree type list:
+                $stype_list = [ 'akna', 'alsó', 'alsósor', 'állomás', 'árok', 'átjáró', 'bányatelep', 'bástya', 'bástyája', 'csónakházak', 'domb', 'dűlő', 'dűlőút', 'emlékpark', 'erdészház', 'erdő', 'erdősor', 'fasor', 'fasora', 'felső', 'felsősor', 'forduló', 'főtér', 'gát', 'gyümölcsös', 'határsor', 'határút', 'hegy', 'iskola', 'kapu', 'kert', 'kertek', 'kolónia', 'körönd', 'körtér', 'körút', 'körútja', 'köz', 'középsor', 'kültelek', 'lakópark', 'lejáró', 'lejtő', 'lépcső', 'lépcsősor', 'liget', 'major', 'MÁV pályaudvar', 'menedékház', 'mélyút', 'oldal', 'őrház', 'őrházak', 'park', 'parkja', 'part', 'pályaudvar', 'puszta', 'rakpart', 'rét', 'sétaút', 'sétány', 'sor', 'sportpálya', 'sugárút', 'szőlőhegy', 'tag', 'tanya', 'tanyák', 'telep', 'tere', 'tető', 'tér', 'turistaház', 'udvar', 'utca', 'utcája', 'út', 'útja', 'üdülőpart', 'vadászház', 'vasútállomás', 'vár', 'vízmű', 'víztároló', 'völgy', 'zártkert', 'zug'];
+
                 $db = $this->get('doctrine.dbal.default_connection');
                 $sql = "SELECT id, title, firstname, lastname, mother_firstname, mother_lastname, zip_code, city, street, street_type, street_number, flat_number FROM person WHERE";
                 // search for ID
@@ -34,23 +37,46 @@ class SearchController extends Controller
                 }
                 else {
                     $search_words = explode(' ', trim($q));
+                    // We cant use FULLTEXT search for fields with very light weights (same values most of the times)
+                    // because the indexer ignores these. Street number and street types are such fields.
+                    // We must use HAVING after the FULLTEXT search to filter these fields.
                     $last = end($search_words);
                     // if the last word is a number, we use that for the street number search
                     if (preg_match('/^\d+(\/|\.|-)?\w*\.?$/', $last)) {
+                        // remove the last element
                         array_pop($search_words);
+                        // also remove any extra chars
                         $last = strtr($last, ['/' => '', '.' => '', ' ' => '']);
                         //$last .= '%';
+                        $last = $db->quote($last);
                     }
                     else {
                         $last = false;
                     }
+                    // check for street types
+                    $street_types = [];
+                    foreach ($search_words as $sk => $sw) {
+                        if (in_array($sw, $stype_list)) {
+                            $street_types[] = $db->quote($sw);
+                            unset($search_words[$sk]);
+                        }
+                    }
+
                     $qr = $db->quote('+' . implode('* +', $search_words) . '*');
 
-                    $sql .= " MATCH (firstname, lastname, street, street_type) AGAINST ({$qr} IN BOOLEAN MODE)";
+                    $sql .= " MATCH (firstname, lastname, street) AGAINST ({$qr} IN BOOLEAN MODE)";
 
                     // if we search for street number
-                    if ($last) {
-                        $sql .= " HAVING street_number = " . $db->quote($last);
+                    if (!empty($last) || !empty($street_types)) {
+                        $xsql = [];
+                        if (!empty($last)) {
+                            $xsql[] = "street_number = " . $last;
+                        }
+                        if (!empty($street_types)) {
+                            $xsql[] = "street_type IN (" . implode(',', $street_types) . ")";
+                        }
+
+                        $sql .= " HAVING " . implode(' AND ', $xsql);
                     }
                 }
                 $sql .= " ORDER BY lastname, firstname LIMIT " . $limit;
