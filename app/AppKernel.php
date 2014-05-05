8f8373f391c0d6c72ace6ee67f3e9751e402e4f2
Bence Meszaros <bence@bencemeszaros.com>
2014-05-05 17:26:07 +0200

    PhpExccel bundle removed

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,7 +24,7 @@ class AppKernel extends Kernel
             new JCSGYK\AdminBundle\JCSGYKAdminBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new MBence\OpenTBSBundle\OpenTBSBundle(),
-            new Liuggio\ExcelBundle\LiuggioExcelBundle(),
+//            new Liuggio\ExcelBundle\LiuggioExcelBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
